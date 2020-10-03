a004a9635171bec1103d3dcddeff77b8e4de75d9
Donovan van der Linde <donovan@overdrive.co.za>
2020-10-03 16:41:38 +0200

    first commit

---
+++ precache-manifest.69e51e36e8572d9f23cf14712733a2b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "219a45b1263cef6792631ccd3691f243",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6178d5a5fd28fbb5b1d4",
+    "url": "/static/css/main.185c0d65.chunk.css"
+  },
+  {
+    "revision": "b250013343b924a5d992",
+    "url": "/static/js/2.0cdcda91.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.0cdcda91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6178d5a5fd28fbb5b1d4",
+    "url": "/static/js/main.dcf1cc21.chunk.js"
+  },
+  {
+    "revision": "9266e46a141649dc80a9",
+    "url": "/static/js/runtime-main.f4d47a17.js"
+  }
+]);
